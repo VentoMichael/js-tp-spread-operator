e57f4caa35fa0896a1909fb98efb688531758843
Vento <Michael.vento@student.hepl.be>
2019-11-28 14:27:01 +0100

    Other solution

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,35 @@
 
+//
+// (function () {
+//     const titleMove = {
+//         cacheDom() {
+//             this.h = document.querySelector('.jump');
+//             this.arrayL = Array.from(this.h.textContent);
+//         },
+//         boucle(){
+//             for (let letter of this.arrayL) {
+//                 const elmSpan = document.createElement('span');
+//                 elmSpan.textContent = letter;
+//                 this.h.appendChild(elmSpan);
+//                 if (letter === ' '){
+//                     elmSpan.textContent = '\u00A0';
+//                 }else{
+//                     elmSpan.textContent = letter;
+//                 }
+//             }
+//         },
+//         init() {
+//             document.documentElement.classList.add('js-enabled');
+//             this.cacheDom();
+//             this.h.textContent ="";
+//             this.boucle();
+//         }
+//     };
+//     titleMove.init();
+// })();
+
+
+
 
 (function () {
     const titleMove = {
@@ -6,20 +37,18 @@
             this.h = document.querySelector('.jump');
             this.arrayL = Array.from(this.h.textContent);
         },
+        boucle(){
+            for (let letter of this.arrayL) {
+                //const elmSpan = document.createElement('span');
+                this.h.insertAdjacentHTML('beforeend',
+                    `<span>${(letter === " " ? "\u00A0" : letter)}</span>`);
+            }
+        },
         init() {
             document.documentElement.classList.add('js-enabled');
             this.cacheDom();
             this.h.textContent ="";
-            for (let letter of this.arrayL) {
-                const elmSpan = document.createElement('span');
-                elmSpan.textContent = letter;
-                this.h.appendChild(elmSpan);
-                if (letter === ' '){
-                    elmSpan.textContent = '\u00A0';
-                }else{
-                    elmSpan.textContent = letter;
-                }
-            }
+            this.boucle();
         }
     };
     titleMove.init();
